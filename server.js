eb113c32e93a6a38dc6c7ea36034383806ff6806
ambadychandran <ambadychandran@gmail.com>
2023-01-06 14:15:17 +0400

    signin and signup functions are added

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var app = express();
 // require modules for authentication
 var passport = require('passport');
 var session = require('express-session');
+//config file
+var env = require('dotenv').config();
+// view template
+var exphbs = require('express-handlebars')
 
 //middle for requests handle
 app.use(express.urlencoded({
@@ -16,16 +20,35 @@ app.use(session({
     resave: true,
     saveUninitialized:true
 }));
+
 // session secret
 app.use(passport.initialize());
 // persistent login sessions
 app.use(passport.session());
 
-//config file
-var env = require('dotenv').config();
-
 //Models
 var models = require("./app/models");
+
+// load the handlebars middlewear
+// app.use(hbs.middleware({
+//     viewPath: path.resolve(__dirname, './app/views'),
+//     layoutsPath: path.resolve(__dirname, './app/views/layouts'),
+//     defaultLayout: 'main'
+//   }))
+
+
+//For Handlebars
+app.set('views', './app/views');
+app.engine('hbs', exphbs.engine({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    layoutsDir: "./app/views/layouts/"
+}));
+app.set('view engine', '.hbs');
+
+//Routes
+var authRoute = require('./app/routes/auth.js')(app);
+
 //Sync Database
 models.sequelize.sync().then(function() {
     console.log('Database looks fine');
