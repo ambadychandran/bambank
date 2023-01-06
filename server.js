5302279e50b95f1acfcf8d3762bcf04043b5428b
ambadychandran <ambadychandran@gmail.com>
2023-01-06 13:27:06 +0400

    sequelize and database configuration added

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
 var express = require('express');
 var app = express();
+// require modules for authentication
+var passport = require('passport');
+var session = require('express-session');
+
+//middle for requests handle
+app.use(express.urlencoded({
+    extended: true
+    })
+);
+app.use(express.json());
+// For Passport
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized:true
+}));
+// session secret
+app.use(passport.initialize());
+// persistent login sessions
+app.use(passport.session());
+
+//config file
+var env = require('dotenv').config();
+
+//Models
+var models = require("./app/models");
+//Sync Database
+models.sequelize.sync().then(function() {
+    console.log('Database looks fine');
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database");
+});
+
 app.get('/', function (req, res) {
     res.send('Welcome to Bambank');
 });
